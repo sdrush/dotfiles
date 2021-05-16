078fdb0b8161f0a3bab3cb0dc435e59287194c33
Shannon Rush <shannon.rush@gmail.com>
2021-05-16 11:14:48 -0500

    Created README.md file and added MacOS section

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,7 @@
 mkdir -p $HOME/.dotfiles
 
 # Set up all our file symlinks
+# ln -sf makes this operation idempotent for files
 ln -sf "$HOME/.dotfiles/zshrc" "$HOME/.zshrc"
 ln -sf "$HOME/.dotfiles/fzf.zsh" "$HOME/.fzf.zsh"
 ln -sf "$HOME/.dotfiles/taskrc" "$HOME/.taskrc"
@@ -11,5 +12,6 @@ ln -sf "$HOME/.dotfiles/viminfo" "$HOME/.viminfo"
 ln -sf "$HOME/.dotfiles/vimrc" "$HOME/.vimrc"
 
 # Set up directory symlinks.
+# Have to use ln -sfn for idempotency with directories 
 ln -sfn "$HOME/.dotfiles/oh-my-zsh/" "$HOME/.oh-my-zsh"
 ln -sfn "$HOME/.dotfiles/vim/" "$HOME/.vim"
