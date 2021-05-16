835ab9f39c436f96ccea3bfe1dbeb01f419c7902
Shannon Rush <shannon.rush@mavenwave.com>
2021-05-16 22:21:44 +0000

    Initial commit of Debian specific bootstrap file.
    Add zplug as a submodule of the dotfiles repo.  Tested package install
    and Starship prompt install.

---
+++ linux/bootstrap.sh
@@ -0,0 +1,13 @@
+################################################
+# Default file name $DOTFILES/linux/bootstrap.sh
+################################################
+
+# First let's sort out what distro we are running on
+distro=`cat /etc/*-release | awk -F= '/^ID=/ {gsub(/"/, ""); print $2}'`
+
+# Source our OS-specific files here
+if [[ "$distro" == "debian" ]]; then
+  source $DOTFILES/linux/debian.bootstrap.sh
+elif [[ "$distro" == "centos" ]]; then
+  source $DOTFILES/linux/centos.bootstrap.sh
+fi
---
+++ linux/debian.bootstrap.sh
@@ -0,0 +1,11 @@
+################################################
+# Default file name $DOTFILES/linux/debian.bootstrap.sh
+################################################
+
+
+# First lets install any local packages we need
+sudo apt-get upgrade -y
+sudo apt-get install lua5.3 -y
+
+# Install or update starship
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
