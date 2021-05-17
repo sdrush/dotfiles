66565ba94b124e755a217d1dd294c40e6228cbf4
Shannon Rush <shannon.rush@gmail.com>
2021-05-17 02:14:51 +0000

    Removing taskwarrior from Linux bootstrap

--- linux/debian.bootstrap.sh
+++ linux/debian.bootstrap.sh
@@ -6,7 +6,6 @@
 # First lets install any local packages we need
 sudo apt-get upgrade -y
 sudo apt-get install lua5.3 -y
-sudo apt-get install task -y
 
 # Install or update starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
