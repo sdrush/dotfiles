c34a58f7fc2bbcbdde29d682edcbfc37eb73a527
Shannon Rush <shannon.rush@gmail.com>
2021-05-18 23:47:06 -0500

    Added comment header to macos alias file

--- macos/aliases.zsh
+++ macos/aliases.zsh
@@ -1,5 +1,17 @@
-# MacOS Specific Aliases
+#!/usr/bin/env zsh
 
+########################################################################
+#
+# Filename: .dotfiles/macos/aliases.zsh 
+#
+# Tl;dr MacOS specific aliases.  Run from zshrc by zplug
+#
+# Author: Shannon Rush (shannondotrushatgmaildotcom)
+# Date: May 2021
+#
+########################################################################
+
+# MacOS Specific Aliases
 alias chrome="open -a 'Google Chrome'"
 alias code="open -a 'Visual Studio Code.app'"
 alias python='/usr/local/bin/python3'
