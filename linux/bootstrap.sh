1635828c89b6af7a7f51d541b7df9e2ae1d4a5c9
Shannon Rush <shannon.rush@gmail.com>
2022-02-02 17:07:52 -0600

    Fix: Starship unattended install

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -15,7 +15,10 @@ sudo pacman install lua5.3
 sudo pacman install taskwarrior
 
 # Install or update starship
-sh -c "$(curl -fsSL https://starship.rs/install.sh) -f"
+#sh -c "$(curl -fsSL https://starship.rs/install.sh) -f"
+sudo wget -O /tmp/starship-install.sh https://starship.rs/install.sh
+sudo chmod 755 /tmp/starship-install.sh
+/tmp/starship-installer.sh -f
 
 # Figure out what distro we are running (centos, debian, fedora)
 #distro=`cat /etc/*-release | awk -F= '/^ID=/ {gsub(/"/, ""); print $2}'`
