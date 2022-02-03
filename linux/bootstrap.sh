fd9f0a29675c274cc928bc6db11f29c73c0fc071
Shannon Rush <shannon.rush@gmail.com>
2022-02-03 17:50:16 -0600

    Feature: moved zplug and it's dependencies to be installed here

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -13,9 +13,18 @@ sudo pacman -Syu
 sudo pacman -S lua5.3
 sudo pacman -S taskwarrior
 sudo pacman -S zplug
+sudo pacman -S direnv
+sudo pacman -S sqlite3
+sudo pacman -S fzf
+sudo pacman -S fonts-powerline
+
+# Clean up some permissions issues with zplug on debian
+if [[ -f "/usr/share/zplug" ]]; then
+    sudo chmod 775 /usr/share/zplug
+    sudo chown root:$USER /usr/share/zplug
+fi
 
 # Install or update starship
-#sh -c "$(curl -fsSL https://starship.rs/install.sh) -f"
 sudo wget -O /tmp/starship-install.sh https://starship.rs/install.sh
 sudo chmod 755 /tmp/starship-install.sh
 /tmp/starship-install.sh -f
