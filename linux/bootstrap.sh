3c70473bde86345fed0e7e6c5846acda6752d28a
Shannon Rush <shannon.rush@gmail.com>
2022-02-03 21:12:12 -0600

    Fix: still having issues with pacman

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -11,7 +11,7 @@ sudo ln -sv /usr/local/bin/pacapt /usr/local/bin/pacman || true
 # Install our packaged dependencies
 export DEBIAN_FRONTEND=noninteractive
 sudo pacman -Syu
-sudo pacman -S --assume-yes --no-install-recommends lua5.3 \
+sudo pacman -S lua5.3 \
     taskwarrior \
     zplug \
     direnv \
