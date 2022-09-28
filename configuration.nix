eba323c21d81a0186d1ece83e0de9188e125233a
sdrush-mw <shannon.rush@mavenwave.com>
2022-09-28 12:14:26 -0500

    Update to latest from Nixpkgs

--- configuration.nix
+++ configuration.nix
@@ -56,6 +56,7 @@
   # Keyboard
   system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToEscape = true;
+  
 
   # Add ability to used TouchID for sudo authentication
   security.pam.enableSudoTouchIdAuth = true;
