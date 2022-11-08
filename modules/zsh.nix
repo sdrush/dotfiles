ead671f19c934d013fda5e62f5a1335b0bda6d71
sdrush-mw <shannon.rush@mavenwave.com>
2022-11-07 20:31:33 -0600

    Update to latest from Nixpkgs 11072022

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -8,6 +8,7 @@
     enableSyntaxHighlighting = true;
     completionInit = "autoload -U compinit && compinit";
     envExtra = "
+    export SSH_AUTH_SOCK=/Users/sdrush/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
       if [[ -n $SSH_CONNECTION ]]; then
         export EDITOR='vim'
       else
