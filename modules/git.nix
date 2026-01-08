d39cd7ee8f4dd58d77e5e3e1dfeb60f09b28fc56
Shannon Rush <shannon.rush@mavenwave.com>
2026-01-07 19:33:51 -0600

    ENHANCEMENT: Moved some GIT settings to the nix git module

--- modules/git.nix
+++ modules/git.nix
@@ -26,12 +26,35 @@ in {
       # the `personal` identity
       user.personal.name = "Shannon Rush";
       user.personal.email = "shannon.rush@gmail.com";
+
+      # Set up out default editor and diff tools
+      core.editor = "code --wait";
+      diff.tool = "vscode";
+      "difftool \"vscode\"".cmd =
+        "code --wait --diff $LOCAL $REMOTE";
+      merge.tool = "vscode";
+      "mergetool \"vscode\"".cmd =
+        "code --wait $MERGED";
+
+      # Other important settings
+      color.ui = "auto";
+      core.autocrlf = "input";
+      fetch.prune = true;
+
+      # Pull/Push Safety
+      pull.rebase = true; # Keeps history clean by rebasing on pull
+      push.autoSetupRemote = true; # Automatically tracks remote branches
+
     };
     # This is optional, as `git identity` will call the `git-identity` script by itself, however
     # setting it up explicitly as an alias gives you autocomplete
     settings.aliases = {
       identity = "! git-identity";
       id = "! git-identity";
+
+      # A Pretty Git Log Graph
+      lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - \
+      %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
     };
   };
 
