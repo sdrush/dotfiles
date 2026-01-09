c515ad5b3a9cf63de5ff658f736faed5be95cc5d
Shannon Rush <shannon.rush@mavenwave.com>
2026-01-08 19:48:13 -0600

    FIXUP: Fix broken lg alias behavior

--- modules/git.nix
+++ modules/git.nix
@@ -45,17 +45,15 @@ in {
       pull.rebase = true; # Keeps history clean by rebasing on pull
       push.autoSetupRemote = true; # Automatically tracks remote branches
 
-    };
     # This is optional, as `git identity` will call the `git-identity` script by itself, however
     # setting it up explicitly as an alias gives you autocomplete
-    settings.aliases = {
-      identity = "! git-identity";
-      id = "! git-identity";
+      alias = {
+        identity = "! git-identity";
+        id = "! git-identity";
 
       # A Pretty Git Log Graph
-      lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - \
-      %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+        lg = "log --graph --abbrev-commit --decorate --format=format:\"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)\" --all";
+      };
     };
   };
-
 }
