d0b398c4b40a17aab4a11b12a48e08227f39407f
Shannon Rush <shannon.rush@mavenwave.com>
2026-01-08 20:45:03 -0600

    UPDATE: Latest nixpkgs 01082026

--- modules/git.nix
+++ modules/git.nix
@@ -50,8 +50,7 @@ in {
       alias = {
         identity = "! git-identity";
         id = "! git-identity";
-
-      # A Pretty Git Log Graph
+    # A Pretty Git Log Graph
         lg = "log --graph --abbrev-commit --decorate --format=format:\"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)\" --all";
       };
     };
