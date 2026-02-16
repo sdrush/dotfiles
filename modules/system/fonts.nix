4de37d849555097950e008b7399f29bdc2abc432
Shannon Rush <shannon.rush@mavenwave.com>
2026-02-15 23:45:00 -0600

    feat(user): modernize come cli tools and optimize Starship prompt
    - Add `uv` to development packages
    - Migrate `nixfmt` to `nixfmt-rfc-style`
    - Update Starship `gcloud` module with environment variable detection

--- modules/system/fonts.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-
-{
-  # Fonts
-  fonts.packages = with pkgs; [
-    nerd-fonts.fira-code
-    nerd-fonts.meslo-lg
-  ];
-}
--- modules/system/users.nix
+++
@@ -1,9 +0,0 @@
-{ user, ... }:
-
-{
-  # User(s)
-  users.users."${user}" = {
-    name = "${user}";
-    home = "/Users/${user}";
-  };
-}
