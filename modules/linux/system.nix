b17ad32b556326cafe86b34fe5764545becfa190
Shannon Rush <shannon.rush@mavenwave.com>
2026-03-04 01:19:40 -0600

    fix: resolve nix.package conflict and clean up unused args

--- modules/linux/system.nix
+++ modules/linux/system.nix
@@ -3,30 +3,28 @@
   ...
 }:
 {
-  config = {
-    nix = {
-      package = pkgs.nix;
-      settings = {
-        experimental-features = [
-          "nix-command"
-          "flakes"
-        ];
-        trusted-users = [
-          "root"
-          "@wheel"
-        ];
-      };
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
     };
-
-    # Basic systemPackages for system-manager
-    environment.systemPackages = with pkgs; [
-      vim
-      git
-      curl
-      wget
-    ];
-
-    # Example: Manage a file via system-manager
-    # system-manager.etc.file."nix/nix.custom.conf".text = "max-jobs = auto\n";
   };
+
+  # Basic systemPackages for system-manager
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    curl
+    wget
+  ];
+
+  # Example: Manage a file via system-manager
+  # system-manager.etc.file."nix/nix.custom.conf".text = "max-jobs = auto\n";
 }
--- modules/system/cachix.nix
+++ modules/system/cachix.nix
@@ -1,12 +1,18 @@
-_:
+{
+  lib,
+  options,
+  ...
+}:
 
 {
-  nix.settings = {
-    substituters = [
-      "https://sdrush.cachix.org"
-    ];
-    trusted-public-keys = [
-      "sdrush.cachix.org-1:g7fLFRwyj6OGcG+qz9PcsOHt1hlwQGE9aBn//xbCm0M="
-    ];
+  config = lib.mkIf (options ? nix) {
+    nix.settings = {
+      substituters = [
+        "https://sdrush.cachix.org"
+      ];
+      trusted-public-keys = [
+        "sdrush.cachix.org-1:g7fLFRwyj6OGcG+qz9PcsOHt1hlwQGE9aBn//xbCm0M="
+      ];
+    };
   };
 }
