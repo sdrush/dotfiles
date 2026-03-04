0d631fc5b3654ae92c61d451351ef41e8f54d882
Shannon Rush <shannon.rush@mavenwave.com>
2026-03-03 21:51:11 -0600

    refactor(zsh): declarative abbreviations and YSU integration
    Refactored [modules/user/abbr.nix](cci:7://file:///Users/sdrush/.dotfiles/modules/user/abbr.nix:0:0-0:0) into a declarative Nix attrset to
    dynamically generate both abbreviations and `YSU_IGNORED_ALIASES`.
    This silences `zsh-you-should-use` warnings for expanded abbreviations
    and common base aliases like `g` (git) and `k` (kubectl).

--- modules/user/abbr.nix
+++ modules/user/abbr.nix
@@ -1,92 +1,118 @@
 { lib, ... }:
 
-{
+let
   # zsh-abbr is a Zsh plugin that expands abbreviations as you type.
   # This provides the best of both worlds: short commands for speed,
   # but full commands in your history and on your screen.
+  #
+  # We define them here in a Nix attrset so we can dynamically
+  # populate YSU_IGNORED_ALIASES to prevent zsh-you-should-use
+  # from complaining about the expanded abbreviations.
+  abbreviations = {
+    # Git
+    gst = "git status";
+    gd = "git diff";
+    ga = "git add";
+    gaa = "git add --all";
+    gco = "git checkout";
+    gcb = "git checkout -b";
+    gp = "git push";
+    gl = "git pull";
+    gc = "git commit -v";
+    gca = "git commit -v -a";
+    gcam = "git commit -m";
+    gcm = "git commit -m";
+    grb = "git rebase";
+    grba = "git rebase --abort";
+    grbc = "git rebase --continue";
+    grbi = "git rebase -i";
+    gsta = "git stash push";
+    gstp = "git stash pop";
+    gstl = "git stash list";
 
+    # Terraform
+    tf = "terraform";
+    tfa = "terraform apply";
+    tfay = "terraform apply -auto-approve";
+    tfc = "terraform console";
+    tfd = "terraform destroy";
+    tfdy = "terraform destroy -auto-approve";
+    tff = "terraform fmt";
+    tfg = "terraform graph";
+    tfi = "terraform init";
+    tfiu = "terraform init -upgrade";
+    tfim = "terraform import";
+    tfo = "terraform output";
+    tfp = "terraform plan";
+    tfpde = "terraform plan --destroy";
+    tfpr = "terraform providers";
+    tfr = "terraform refresh";
+    tfsh = "terraform show";
+    tft = "terraform taint";
+    tfut = "terraform untaint";
+    tfv = "terraform validate";
+    tfw = "terraform workspace";
+    tfs = "terraform state";
+    tffu = "terraform force-unlock";
+    tfwst = "terraform workspace select";
+    tfwsw = "terraform workspace show";
+    tfwde = "terraform workspace delete";
+    tfwls = "terraform workspace list";
+    tfwnw = "terraform workspace new";
+    tfssw = "terraform state show";
+    tfsls = "terraform state list";
+    tfsmv = "terraform state mv";
+    tfspl = "terraform state pull";
+    tfsph = "terraform state push";
+    tfsrm = "terraform state rm";
+
+    # Others
+    kctx = "kubectx";
+    kns = "kubens";
+    tg = "terragrunt";
+    reborn = "just rebuild";
+    dotcfg = "\\$EDITOR \\$DOTFILES";
+    config = "git --git-dir=\\$HOME/.cfg/ --work-tree=\\$HOME";
+    mp = "man-preview";
+    myip = "curl http://ipecho.net/plain; echo";
+    ports = "netstat -tulanp";
+    h = "history";
+    l = "ls";
+    pip = "python -m pip";
+    wttr = "curl wttr.in";
+
+    # Task Management
+    "in" = "task add +in";
+    think = "tickle +1d";
+    tick = "tickle";
+    rnr = "read_and_review";
+    rnd = "task add +rnd +next +@computer +@online";
+  };
+
+  # Generate YSU ignore list
+  # We add 'g' (git) and 'k' (kubectl) specifically because they are common base aliases
+  # that trigger fallback warnings when abbreviations expand to their full forms.
+  ysu_ignores = lib.concatStringsSep " " (
+    (lib.attrNames abbreviations)
+    ++ [
+      "g"
+      "k"
+    ]
+  );
+
+  # Generate abbr add commands
+  abbr_commands = lib.concatStringsSep "\n      " (
+    lib.mapAttrsToList (name: value: "abbr add --quiet ${name}='${value}'") abbreviations
+  );
+in
+{
   programs.zsh.initContent = lib.mkAfter ''
+    # Silence YSU for abbreviations
+    export YSU_IGNORED_ALIASES=(${ysu_ignores})
+
     # We use zsh-defer to ensure these are added AFTER the plugin is loaded by Home Manager.
-    # We also use --quiet to suppress 'already exists' warnings.
     zsh-defer -c "
-      # Terraform Abbreviations
-      abbr add --quiet tf=terraform
-      abbr add --quiet tfa='terraform apply'
-      abbr add --quiet tfay='terraform apply -auto-approve'
-      abbr add --quiet tfc='terraform console'
-      abbr add --quiet tfd='terraform destroy'
-      abbr add --quiet tfdy='terraform destroy -auto-approve'
-      abbr add --quiet tff='terraform fmt'
-      abbr add --quiet tfg='terraform graph'
-      abbr add --quiet tfi='terraform init'
-      abbr add --quiet tfiu='terraform init -upgrade'
-      abbr add --quiet tfim='terraform import'
-      abbr add --quiet tfo='terraform output'
-      abbr add --quiet tfp='terraform plan'
-      abbr add --quiet tfpde='terraform plan --destroy'
-      abbr add --quiet tfpr='terraform providers'
-      abbr add --quiet tfr='terraform refresh'
-      abbr add --quiet tfsh='terraform show'
-      abbr add --quiet tft='terraform taint'
-      abbr add --quiet tfut='terraform untaint'
-      abbr add --quiet tfv='terraform validate'
-      abbr add --quiet tfw='terraform workspace'
-      abbr add --quiet tfs='terraform state'
-      abbr add --quiet tffu='terraform force-unlock'
-      abbr add --quiet tfwst='terraform workspace select'
-      abbr add --quiet tfwsw='terraform workspace show'
-      abbr add --quiet tfwde='terraform workspace delete'
-      abbr add --quiet tfwls='terraform workspace list'
-      abbr add --quiet tfwnw='terraform workspace new'
-      abbr add --quiet tfssw='terraform state show'
-      abbr add --quiet tfsls='terraform state list'
-      abbr add --quiet tfsmv='terraform state mv'
-      abbr add --quiet tfspl='terraform state pull'
-      abbr add --quiet tfsph='terraform state push'
-      abbr add --quiet tfsrm='terraform state rm'
-
-      # Git Abbreviations
-      abbr add --quiet gst='git status'
-      abbr add --quiet gd='git diff'
-      abbr add --quiet ga='git add'
-      abbr add --quiet gaa='git add --all'
-      abbr add --quiet gco='git checkout'
-      abbr add --quiet gcb='git checkout -b'
-      abbr add --quiet gp='git push'
-      abbr add --quiet gl='git pull'
-      abbr add --quiet gc='git commit -v'
-      abbr add --quiet gca='git commit -v -a'
-      abbr add --quiet gcam='git commit -m'
-      abbr add --quiet gcm='git commit -m'
-      abbr add --quiet grb='git rebase'
-      abbr add --quiet grba='git rebase --abort'
-      abbr add --quiet grbc='git rebase --continue'
-      abbr add --quiet grbi='git rebase -i'
-      abbr add --quiet gsta='git stash push'
-      abbr add --quiet gstp='git stash pop'
-      abbr add --quiet gstl='git stash list'
-
-      # Other Abbreviations
-      abbr add --quiet kctx=kubectx
-      abbr add --quiet kns=kubens
-      abbr add --quiet tg=terragrunt
-      abbr add --quiet reborn='just rebuild'
-      abbr add --quiet dotcfg='\$EDITOR \$DOTFILES'
-      abbr add --quiet config='git --git-dir=\$HOME/.cfg/ --work-tree=\$HOME'
-      abbr add --quiet mp='man-preview'
-      abbr add --quiet myip='curl http://ipecho.net/plain; echo'
-      abbr add --quiet ports='netstat -tulanp'
-      abbr add --quiet h='history'
-      abbr add --quiet l='ls'
-      abbr add --quiet pip='python -m pip'
-      abbr add --quiet wttr='curl wttr.in'
-
-      # Task Management
-      abbr add --quiet in='task add +in'
-      abbr add --quiet think='tickle +1d'
-      abbr add --quiet tick='tickle'
-      abbr add --quiet rnr='read_and_review'
-      abbr add --quiet rnd='task add +rnd +next +@computer +@online'
+      ${abbr_commands}
     "
   '';
 }
