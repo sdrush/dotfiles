3bccd095092ae78e14e8bb36034b3313593a2342
Shannon Rush <shannon.rush@gmail.com>
2026-03-04 11:52:10 -0600

    fix: use extra-substituters and whitelist new vim cves
    - Use extra-substituters and extra-trusted-public-keys in cachix module to avoid overriding default NixOS caches
    - Add CVE-2026-28417 and CVE-2026-28418 to vulnix whitelist for vim

--- modules/system/cachix.nix
+++ modules/system/cachix.nix
@@ -7,10 +7,10 @@
 {
   config = lib.mkIf (options ? nix) {
     nix.settings = {
-      substituters = [
+      extra-substituters = [
         "https://sdrush.cachix.org"
       ];
-      trusted-public-keys = [
+      extra-trusted-public-keys = [
         "sdrush.cachix.org-1:g7fLFRwyj6OGcG+qz9PcsOHt1hlwQGE9aBn//xbCm0M="
       ];
     };
