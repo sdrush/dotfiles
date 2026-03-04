16538e7767a3a1a13f6c805bcdfb905eaeabdc56
Shannon Rush <shannon.rush@mavenwave.com>
2026-03-04 01:19:40 -0600

    fix: resolve nix.package conflict and clean up unused args

--- modules/linux/system.nix
+++ modules/linux/system.nix
@@ -1,30 +1,33 @@
 {
   pkgs,
+  lib,
   ...
 }:
 {
-  nix = {
-    package = pkgs.nix;
-    settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      trusted-users = [
-        "root"
-        "@wheel"
-      ];
+  config = {
+    nix = {
+      package = lib.mkForce pkgs.nix;
+      settings = {
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        trusted-users = [
+          "root"
+          "@wheel"
+        ];
+      };
     };
+
+    # Basic systemPackages for system-manager
+    environment.systemPackages = with pkgs; [
+      vim
+      git
+      curl
+      wget
+    ];
+
+    # Example: Manage a file via system-manager
+    # system-manager.etc.file."nix/nix.custom.conf".text = "max-jobs = auto\n";
   };
-
-  # Basic systemPackages for system-manager
-  environment.systemPackages = with pkgs; [
-    vim
-    git
-    curl
-    wget
-  ];
-
-  # Example: Manage a file via system-manager
-  # system-manager.etc.file."nix/nix.custom.conf".text = "max-jobs = auto\n";
 }
